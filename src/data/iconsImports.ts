f2d9bf48f3b693571b995c93c64ff12ceea731cb
Kagnana <kagnana.ith@ecole-isitech.fr>
2024-09-03 22:26:03 +0200

    fix: ts file for icon imports

--- src/data/iconsImports.ts
+++ src/data/iconsImports.ts
@@ -1,19 +1,19 @@
-import reactIcon from "../assets/React-icon.svg.png"
-import jsIcon from "../assets/JavaScript-logo.png"
-import tsIcon from "../assets/TypeScript-logo.png"
-import mongodbIcon from "../assets/mongoDB-icon.svg.png"
-import javaLogo from "../assets/Java_Logo.svg"
-import swiftLogo from "../assets/swift.png"
-import swiftUiLogo from "../assets/swiftui.png"
-import gitLogo from "../assets/Git-logo.png"
-import dockerLogo from "../assets/docker-logo.svg"
-import cSharpLogo from "../assets/Csharp-logo.png"
-import mySQLLogo from "../assets/MySQL-logo.png"
-import winFormsLogo from "../assets/WinFormLogo.png"
-import phpLogo from "../assets/php-icon.png"
-import htmlLogo from "../assets/html-icon.png"
-import cssLogo from "../assets/css-icon.png"
-import azureDevOpsLogo from "../assets/azure-devops-logo.png"
+import reactIcon from "../assets/React-icon.svg.png";
+import jsIcon from "../assets/JavaScript-logo.png";
+import tsIcon from "../assets/TypeScript-logo.png";
+import mongodbIcon from "../assets/mongoDB-icon.svg.png";
+import javaLogo from "../assets/Java_Logo.svg";
+import swiftLogo from "../assets/swift.png";
+import swiftUiLogo from "../assets/swiftui.png";
+import gitLogo from "../assets/Git-logo.png";
+import dockerLogo from "../assets/docker-logo.svg";
+import cSharpLogo from "../assets/Csharp-logo.png";
+import mySQLLogo from "../assets/MySQL-logo.png";
+import winFormsLogo from "../assets/WinFormLogo.png";
+import phpLogo from "../assets/php-icon.png";
+import htmlLogo from "../assets/html-icon.png";
+import cssLogo from "../assets/css-icon.png";
+import azureDevOpsLogo from "../assets/azure-devops-logo.png";
 
 export const icons = {
     react: reactIcon,
