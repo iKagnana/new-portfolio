d025ae8a588e4e083192d78bc49c32a081ded506
Kagnana <kagnana.ith@ecole-isitech.fr>
2024-09-03 22:23:47 +0200

    add first pages intro and about me

--- src/data/iconsImports.ts
+++ src/data/iconsImports.ts
@@ -1,4 +1,3 @@
----
 import reactIcon from "../assets/React-icon.svg.png"
 import jsIcon from "../assets/JavaScript-logo.png"
 import tsIcon from "../assets/TypeScript-logo.png"
@@ -15,5 +14,22 @@ import phpLogo from "../assets/php-icon.png"
 import htmlLogo from "../assets/html-icon.png"
 import cssLogo from "../assets/css-icon.png"
 import azureDevOpsLogo from "../assets/azure-devops-logo.png"
----
 
+export const icons = {
+    react: reactIcon,
+    javascript: jsIcon,
+    typescript: tsIcon,
+    mongoDB: mongodbIcon,
+    java: javaLogo,
+    swift: swiftLogo,
+    swiftUi: swiftUiLogo,
+    git: gitLogo,
+    docker: dockerLogo,
+    cSharp: cSharpLogo,
+    mySQL: mySQLLogo,
+    winForms: winFormsLogo,
+    php: phpLogo,
+    html: htmlLogo,
+    css: cssLogo,
+    azureDevops: azureDevOpsLogo
+}
